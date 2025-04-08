a6ff2f0835d3782e51cdcf4b5662652db20a6473
Rusty-spotted cat <skwjdgh2@hotmail.com>
2025-04-08 17:33:03 +0900

    Add files via upload

---
+++ 250408Cclass/고급 포인터/example.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main()
+{
+	int* p;
+	int i = 0 ;
+	int hap = 0;
+	int input;
+
+	printf("입력할 개수는? : ");
+	scanf("%d", input);
+
+	p = (int*)malloc(sizeof(int) * cnt);
+
+	for()
+
+
+
+
+
+	return 0;
+}
